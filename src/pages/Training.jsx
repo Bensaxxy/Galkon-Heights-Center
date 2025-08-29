6675d3119184d2ac9183890a589f009a1cae4ffb
Oluwasegun Benjamin Tobi <benjaminsegun38@gmail.com>
2025-08-29 19:09:14 +0100

    Update EmailJS template ID in Training component for training enrollment

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -28,7 +28,7 @@ const Training = () => {
     emailjs
       .send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        "template_vlu21fj", // your EmailJS template ID
+        import.meta.env.VITE_EMAILJS_TEMPLATE_TRAINING_ID, // your EmailJS template ID
         templateParams,
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
